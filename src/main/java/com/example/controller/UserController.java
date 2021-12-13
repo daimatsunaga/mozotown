edab4ab1ca8463ff668852809c79a751edfd8238
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-12-13 17:57:07 +0900

    UserControllerの作成

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.controller;
+
+import java.sql.Date;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.UserInsertForm;
+import com.example.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@ModelAttribute
+	public UserInsertForm setUpForm() {
+		return new UserInsertForm();
+	}
+	@GetMapping("")
+	public String toTopPage() {
+		return "top_page";
+	}
+	
+	@GetMapping("/toInsert")
+	public String toInsert() {
+		return "register_user";
+	}
+	
+	
+	@PostMapping("/insert")
+	public String insert(UserInsertForm form) {
+		//フォームの値をdomainにコピー
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		
+		//フォームから送られてきた年月日をDate型に変換しuserにセット
+		String date = form.getYear() + "-" + form.getMonth() + "-" + form.getDay();
+		Date birthday = Date.valueOf(date);
+		user.setBirthday(birthday);
+		
+		System.out.println(user);
+		
+		//insertメソッドを呼び出す
+		service.insert(user);
+		
+		return "top_page";
+	}
+ }
