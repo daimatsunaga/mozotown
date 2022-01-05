a223d4d63f9553fd3e1e32a0f2ea0e4384c5d9fb
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-05 10:22:09 +0900

    カートに遷移する際の分岐の設定

--- src/main/java/com/example/controller/CartController.java
+++ src/main/java/com/example/controller/CartController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,10 +20,14 @@ public class CartController {
 	@Autowired
 	private CartService service;
 	
-	@GetMapping("")
-	public String toCart(Model model) {
-		List<Cart> cartList = service.showCart(1);
-		service.showCart(1).forEach(i -> System.out.println(i));
+	@PostMapping("")
+	public String toCart(Model model, Integer userId) {
+		if(userId == null) {
+			return "redirect:/user/toLogin";
+		}
+		
+		List<Cart> cartList = service.showCart(userId);
+//		service.showCart(1).forEach(i -> System.out.println(userId));
 		
 		//カート内の商品の合計金額を計算
 		Integer totalPrice = 0;
