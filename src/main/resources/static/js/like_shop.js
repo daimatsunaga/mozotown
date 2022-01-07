221fd8247692671b29a5d515a7a8ecfc309cd6ec
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-07 10:27:47 +0900

    軽微な修正

--- src/main/resources/static/js/like_shop.js
+++ src/main/resources/static/js/like_shop.js
@@ -2,8 +2,6 @@
 $(function() {
   //お気に入りに追加
   $(document).on('click', '#likes_shop_btn', function () {
-  	$('#likes_shop_btn').hide();
-  	$('#delete_likes_shop_btn').show();
     $.ajax({
         url: 'http://localhost:8080/like/shop',
         dataType: 'text',
@@ -14,10 +12,12 @@ $(function() {
 		type: 'post',
         async: true
     }).done(function() {
+    	$('#likes_shop_btn').hide();
+  		$('#delete_likes_shop_btn').show();
     	alert('お気に入りに追加しました');
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
       // 失敗時には、その旨をダイアログ表示
-      alert('正しい結果を得られませんでした。');
+      alert('ログイン後、使用可能になります');
       console.log('XMLHttpRequest : ' + XMLHttpRequest.status);
       console.log('textStatus     : ' + textStatus);
       console.log('errorThrown    : ' + errorThrown.message);
@@ -26,8 +26,6 @@ $(function() {
   
   	//お気に入りから削除
     $(document).on('click', '#delete_likes_shop_btn', function () {
-  	$('#delete_likes_shop_btn').hide();
-  	$('#likes_shop_btn').show();
     $.ajax({
         url: 'http://localhost:8080/like/deleteLikeShop',
         dataType: 'text',
@@ -38,6 +36,8 @@ $(function() {
 		type: 'post',
         async: true
     }).done(function() {
+      	$('#delete_likes_shop_btn').hide();
+  		$('#likes_shop_btn').show();
     	alert('お気に入りから削除しました');
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
       // 失敗時には、その旨をダイアログ表示
