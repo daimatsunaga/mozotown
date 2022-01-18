c93ed5783d08c5553f877987b86c1fed9e9bb0c0
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-01-18 18:05:21 +0900

    OrderFormの作成

---
+++ src/main/java/com/example/form/OrderForm.java
@@ -0,0 +1,118 @@
+package com.example.form;
+
+import java.sql.Date;
+
+
+public class OrderForm {
+	
+	private Integer userId;
+	
+	private Integer totalPrice;
+	
+	private String destinationName;
+	
+	private String destinationNameKana;
+	
+	private String destinationZipCode;
+	
+	private String destinationAddress;
+	
+	private String destinationPhonenumber;
+	
+	private Date deliveryDate;
+	
+	private Integer deliveryTime;
+	
+	private Integer paymentMethodId;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationNameKana() {
+		return destinationNameKana;
+	}
+
+	public void setDestinationNameKana(String destinationNameKana) {
+		this.destinationNameKana = destinationNameKana;
+	}
+
+	public String getDestinationZipCode() {
+		return destinationZipCode;
+	}
+
+	public void setDestinationZipCode(String destinationZipCode) {
+		this.destinationZipCode = destinationZipCode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationPhonenumber() {
+		return destinationPhonenumber;
+	}
+
+	public void setDestinationPhonenumber(String destinationPhonenumber) {
+		this.destinationPhonenumber = destinationPhonenumber;
+	}
+
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public Integer getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(Integer deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public Integer getPaymentMethodId() {
+		return paymentMethodId;
+	}
+
+	public void setPaymentMethodId(Integer paymentMethodId) {
+		this.paymentMethodId = paymentMethodId;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderForm [userId=" + userId + ", totalPrice=" + totalPrice + ", destinationName=" + destinationName
+				+ ", destinationNameKana=" + destinationNameKana + ", destinationZipCode=" + destinationZipCode
+				+ ", destinationAddress=" + destinationAddress + ", destinationPhonenumber=" + destinationPhonenumber
+				+ ", deliveryDate=" + deliveryDate + ", deliveryTime=" + deliveryTime + ", paymentMethodId="
+				+ paymentMethodId + "]";
+	}
+	
+	
+}
