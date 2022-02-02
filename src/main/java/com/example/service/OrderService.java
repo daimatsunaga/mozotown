01199a72b0e1c6051ff391126c6cfb4bda703ebe
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-02-02 21:47:55 +0900

    APIの呼び出しを追加

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,11 +1,17 @@
 package com.example.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.domain.Order;
 import com.example.repository.CartMapper;
 import com.example.repository.OrderMapper;
+
+import jp.pay.Payjp;
+import jp.pay.model.Charge;
 
 @Service
 public class OrderService {
@@ -21,6 +27,22 @@ public class OrderService {
 	
 	public void orderConfirm(Order order) {
 		orderMapper.orderConfirm(order);
+		
+		//クレジットカードAPI
+		if(order.getPaymentMethodId() == 2) {
+	        Payjp.apiKey = "sk_test_dff77135059a6ff8b051cd7a";
+	        Map<String, Object> chargeMap = new HashMap<String, Object>();
+	        chargeMap.put("amount", order.getTotalPrice());
+	        chargeMap.put("currency", "jpy");
+	        chargeMap.put("card", order.getToken());
+	        try {
+	            Charge charge = Charge.create(chargeMap);
+	            System.out.println(charge);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		}
+        
 		cartMapper.deleteByUserId(order.getUserId());
 		cartService.sumOfCart();
 	}
